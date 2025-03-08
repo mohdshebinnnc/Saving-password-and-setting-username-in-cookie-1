6e82364248a09a1e020ed9f9cc4d53b66f51dd9c
shebin <mohammedshebinc92@gmail.com>
2025-03-08 10:40:55 +0530

    cookies

--- script.js
+++ script.js
@@ -15,9 +15,17 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   // 1. Get the value of the 'count' cookie
+  let count = getCookie('count');
   // 2. If the cookie exists, increment the value and update the cookie
+  if(count !==null){
+    count=parseInt(count)+1
+  }
   // 3. If the cookie does not exist, create it and set the value to 1
+  else{
+    count=1;
+  }
+  setCookie('count',count,7)
   // 4. Display the count on the webpage
-
-  // your code here
+  document.getElementById('countDisplay').textContent=`Page load Count:${count}`
+  
 });
